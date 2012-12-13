f2953ebde8c6ab72c2e49c6fe775b95ed36b0122
René van den Berg <r.van.den.berg@ogd.nl>
2012-12-14 00:52:06 +0100

    Add a test helper
    Again, strongly inspired by activerecord-deprecated_finders

---
+++ test/helper.rb
@@ -0,0 +1,27 @@
+require 'bundler/setup'
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'active_record'
+require 'active_record/like'
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+
+ActiveRecord::Schema.verbose = false
+ActiveRecord::Schema.define do
+  create_table :posts do |t|
+    t.string :title
+    t.string :category
+  end
+
+  create_table :comments do |t|
+    t.string :title
+    t.references :post
+  end
+end
+
+class Post < ActiveRecord::Base
+  has_many :comments
+end
+
+class Comment < ActiveRecord::Base
+end
