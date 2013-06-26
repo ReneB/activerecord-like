6ed4b888f90e6b933817d158d39611d576cecda0
Rene van den Berg <rene.vandenberg@ogd.nl>
2013-06-27 01:09:45 +0200

    Extract shared code into private function. See how Code Climate likes that :-)

--- lib/active_record/like.rb
+++ lib/active_record/like.rb
@@ -4,17 +4,18 @@ module ActiveRecord
   module QueryMethods
     module Like
       def like(opts, *rest)
-        @scope.tap do |s|
-          s.where_values += s.send(:build_where, opts, *rest).map do |r|
-            Arel::Nodes::Matches.new(r.left, r.right)
-          end
-        end
+        chain_node(Arel::Nodes::Matches, opts, *rest)
       end
 
       def not_like(opts, *rest)
+        chain_node(Arel::Nodes::DoesNotMatch, opts, *rest)
+      end
+
+    private
+      def chain_node(node_type, opts, *rest)
         @scope.tap do |s|
           s.where_values += s.send(:build_where, opts, *rest).map do |r|
-            Arel::Nodes::DoesNotMatch.new(r.left, r.right)
+            node_type.new(r.left, r.right)
           end
         end
       end
