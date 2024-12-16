171ada0303b269da6a9f9c00a40aae4e499b141a
PikachuEXE <git@pikachuexe.net>
2024-12-16 11:34:34 +0800

    $ Remove deprecated usage of methods that will fail in Minitest 6

--- test/unit/not_like_test.rb
+++ test/unit/not_like_test.rb
@@ -5,7 +5,7 @@ describe ActiveRecord::QueryMethods::WhereChain do
     it "creates an Arel DoesNotMatch node in the relation" do
       relation = Post.where.not_like(title: '')
 
-      relation.where_clause.send(:predicates).first.must_be_instance_of(Arel::Nodes::DoesNotMatch)
+      _(relation.where_clause.send(:predicates).first).must_be_instance_of(Arel::Nodes::DoesNotMatch)
     end
 
     describe "the Arel Node" do
@@ -18,19 +18,22 @@ describe ActiveRecord::QueryMethods::WhereChain do
       end
 
       it "has the attribute as the left operand" do
-        @first_predicate.left.name.must_equal @attribute
+        _(@first_predicate.left.name).must_equal @attribute
       end
 
       it "has the value as the right operand" do
         # Rails 5.0 & 5.1
         first_bind = if @relation.where_clause.respond_to?(:binds)
           @relation.where_clause.send(:binds).first
+        elsif @first_predicate.right.value.is_a?(String)
+          # Rails 7.0+
+          @first_predicate.right
         else
           # Rails 5.2
           @first_predicate.right.value
         end
 
-        first_bind.value.must_equal @value
+        _(first_bind.value).must_equal @value
       end
     end
   end
